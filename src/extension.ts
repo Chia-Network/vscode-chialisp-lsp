dca963e192f6d3727764a10851a99a631aa3e195
arty <art.yerkes@gmail.com>
2022-08-09 16:47:36 -0700

    Start of a lsp (language server) client for chialisp

---
+++ src/extension.ts
@@ -0,0 +1,93 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import * as path from 'path';
+import {
+    ExecutableOptions,
+    LanguageClient,
+    LanguageClientOptions,
+    Logger,
+    RevealOutputChannelOn,
+    ServerOptions,
+    StaticFeature,
+    TransportKind,
+} from 'vscode-languageclient/node';
+
+var usingLangClient: LanguageClient|undefined = undefined;
+
+export class WorkaroundFeature implements StaticFeature {
+    fillClientCapabilities(capabilities: any): void {
+        capabilities.workspace.workspaceEdit = { documentChanges: true };
+    }
+    initialize(): void {
+    }
+    dispose(): void {
+    }
+}
+
+async function activateServer(context: vscode.ExtensionContext) {
+    const workspaceClientInstanceId = 'chialisp';
+    const outputChannel: vscode.OutputChannel = vscode.window.createOutputChannel(workspaceClientInstanceId);
+    const serverExecutable = 'chia-lsp';
+
+    // Register associations
+    const exeOptions: ExecutableOptions = {
+        cwd: undefined,
+        env: { ... process.env }
+    };
+
+    const serverArgs: string[] = [];
+    const debugArgs = ['-d'];
+
+    const serverOptions: ServerOptions = {
+        run: {command: serverExecutable, transport: TransportKind.stdio, args: serverArgs, options: exeOptions},
+        debug: {command: serverExecutable, transport: TransportKind.stdio, args: debugArgs, options: exeOptions}
+    };
+
+    const fileToExaminePattern = '**/*';
+    const clientOptions: LanguageClientOptions = {
+        // This constrains when the lsp receives a notification that a file should be looked at.
+        documentSelector: [{scheme: 'file', language: 'chialisp'}],
+        // We can use a configuration setting to indicate workspace level
+        // include dirs.
+        synchronize: {
+            configurationSection: 'chialisp',
+            fileEvents: vscode.workspace.createFileSystemWatcher('**/*')
+        },
+
+        diagnosticCollectionName: workspaceClientInstanceId,
+        revealOutputChannelOn: RevealOutputChannelOn.Never,
+        outputChannel,
+        outputChannelName: workspaceClientInstanceId,
+        middleware: {
+            // We'll have to learn what goes here
+        },
+        workspaceFolder: undefined,
+        initializationOptions: {
+            customData: 'here it is'
+        }
+    };
+
+    const langClient = new LanguageClient('chialisp', workspaceClientInstanceId, serverOptions, clientOptions);
+    langClient.registerFeature(new WorkaroundFeature());
+    langClient.registerProposedFeatures();
+    langClient.start();
+
+    // Share with deactivate
+    usingLangClient = langClient;
+}
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the c`ommand is executed
+export async function activate(context: vscode.ExtensionContext) {
+    activateServer(context);
+}
+
+// this method is called when your extension is deactivated
+export async function deactivate() {
+    // Stop service
+    if (usingLangClient) {
+        await usingLangClient.stop();
+    }
+}
