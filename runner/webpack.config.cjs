d2a2be77bb45f641f6c19f6758ae0e7aafba05fe
arty <art.yerkes@gmail.com>
2023-08-16 03:07:07 -0700

    Explicitly set node target

--- runner/webpack.config.cjs
+++ runner/webpack.config.cjs
@@ -3,6 +3,7 @@ var webpack = require('webpack');
 
 module.exports = {
     mode: 'production',
+    target: 'node',
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'runner.js'
