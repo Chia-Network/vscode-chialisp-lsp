8f0e93ac317c1b5d5eb2f7551b2e407d954ad432
arty <art.yerkes@gmail.com>
2026-04-21 13:26:15 -0700

    add test of renaming

--- wasm/src/tests/mod.rs
+++ wasm/src/tests/mod.rs
@@ -1971,7 +1971,6 @@ fn test_sem_tok_for_module_style() {
     let r2 = lsp
         .handle_message(&sem_tok)
         .expect("should be ok to send sem tok");
-    eprintln!("msg {}", get_msg_params(&r2[0]));
     let decoded_tokens: SemanticTokens = serde_json::from_str(&get_msg_params(&r2[0])).unwrap();
     assert_eq!(
         decoded_tokens.data,
@@ -2042,6 +2041,97 @@ fn test_sem_tok_for_module_style() {
             SemanticToken {
                 delta_line: 0,
                 delta_start: 2,
+                length: 1,
+                token_type: TK_PARAMETER_IDX,
+                token_modifiers_bitset: 0,
+            },
+        ]
+    );
+}
+
+#[test]
+fn test_sem_tok_for_module_style_reverse_function() {
+    let mut lsp = LSPServiceProvider::new(
+        Rc::new(FSFileReader::new()),
+        Rc::new(EPrintWriter::new()),
+        true,
+    );
+    lsp.set_workspace_root(PathBuf::from(r"."));
+    lsp.set_config(ConfigJson {
+        include_paths: vec!["../resources/tests".to_string()],
+    });
+    let file = "file://./test.cl".to_string();
+    let open_msg = make_did_open_message(
+        &file,
+        1,
+        indoc! {"
+(import std.reverse exposing (reverse as rev))
+(include *standard-cl-25*)
+(export (X) (rev X))"}
+        .to_string(),
+    );
+    let sem_tok = make_get_semantic_tokens_msg(&file, 2);
+    lsp.handle_message(&open_msg)
+        .expect("should be ok to take open msg");
+    let r2 = lsp
+        .handle_message(&sem_tok)
+        .expect("should be ok to send sem tok");
+    let decoded_tokens: SemanticTokens = serde_json::from_str(&get_msg_params(&r2[0])).unwrap();
+    assert_eq!(
+        decoded_tokens.data,
+        vec![
+            SemanticToken {
+                delta_line: 0,
+                delta_start: 1,
+                length: 6,
+                token_type: TK_KEYWORD_IDX,
+                token_modifiers_bitset: 0,
+            },
+            SemanticToken {
+                delta_line: 0,
+                delta_start: 7,
+                length: 10,
+                token_type: TK_VARIABLE_IDX,
+                token_modifiers_bitset: 0,
+            },
+            SemanticToken {
+                delta_line: 1,
+                delta_start: 1,
+                length: 7,
+                token_type: TK_KEYWORD_IDX,
+                token_modifiers_bitset: 0,
+            },
+            SemanticToken {
+                delta_line: 0,
+                delta_start: 8,
+                length: 16,
+                token_type: TK_STRING_IDX,
+                token_modifiers_bitset: 0,
+            },
+            SemanticToken {
+                delta_line: 1,
+                delta_start: 1,
+                length: 6,
+                token_type: TK_KEYWORD_IDX,
+                token_modifiers_bitset: 0,
+            },
+            SemanticToken {
+                delta_line: 0,
+                delta_start: 8,
+                length: 1,
+                token_type: TK_PARAMETER_IDX,
+                token_modifiers_bitset: 1,
+            },
+            SemanticToken {
+                delta_line: 0,
+                delta_start: 4,
+                length: 3,
+                token_type: TK_FUNCTION_IDX,
+                token_modifiers_bitset: 0,
+            },
+            SemanticToken {
+                delta_line: 0,
+                delta_start: 4,
                 length: 1,
                 token_type: TK_PARAMETER_IDX,
                 token_modifiers_bitset: 0,
